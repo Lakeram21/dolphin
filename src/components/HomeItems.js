d4ba15828a5a72f2f3ddc32af6387a3aedc894eb
Lakeram Narine <nar19002@byui.edu>
2021-10-01 19:38:57 -0600

    added axios to pull from db

--- src/components/HomeItems.js
+++ src/components/HomeItems.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Projector from '../pictures/projector.jpg'
 
 import "./Items.css"
@@ -8,56 +10,35 @@ import "./Items.css"
 
 function HomeItems() {
   // console.log("project ----",Projector)
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      name: "Projector",
-      price: 23500,
-      description: "this is a projector"
-    },
-    {
-      id: 2,
-      name: "Projector-hp",
-      price: 27500,
-      description: "this is a projector"
-    },
-    {
-      id: 3,
-      name: "Projector-dell",
-      price: 23500,
-      description: "this is a projector",
-      images: ""
-    }, {
-      id: 4,
-      name: "Projector-dell",
-      price: 23500,
-      description: "this is a projector"
-    }, {
-      id: 5,
-      name: "Projector-dell",
-      price: 23500,
-      description: "this is a projector"
-    }, {
-      id: 6,
-      name: "Projector-dell",
-      price: 23500,
-      description: "this is a projector"
-    }
+  const [items, setItems] = useState([]);
+  const getProduct = () => {
+    // the backend and the front end are both running on different server
+    // so when the client clicke on the button a post is send to the backend
+    // 
+    axios.get('http://localhost:5000/api/product', { headers: { 'Authorization': `token` } }).then((response) => {
+      const queueInfomation = response.data
+      setItems(queueInfomation)
+      console.log(`que information ${queueInfomation}`)
+      // setqueueInfo({ ...queueInfo, queueId: queueInfomatio })
 
-  ]);
+    })
+  }
+  useEffect(() => {
+    getProduct();
+  }, []);
 
   return (
     <div className="items">
-      
+
       {items.map((item) => (
         <div className="item">
           {/* \src\pictures\v13h010l49.png */}
-          
-          <img src= "https://media.wired.com/photos/598e35fb99d76447c4eb1f28/m aster/pass/phonepicutres-TA.jpg" alt='logo' height='193'/>
-          <h1>{item.name}</h1>
-          {/* <h1>Item id: {item.id}</h1>
-          <h1>{item.price}</h1>
-          <h1>{item.description}</h1> */}
+
+          <img src="https://media.wired.com/photos/598e35fb99d76447c4eb1f28/m aster/pass/phonepicutres-TA.jpg" alt='logo' height='193' />
+          <h1> ProductName :{item.productName}</h1>
+          <h1>ProductCode: {item.productCode}</h1>
+          <h1>ProductPrice{item.price}</h1>
+          <h1>{item.description}</h1>
 
         </div>
       ))}
